155718ccc99af907bd82438e1f6028022a261f60
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-10-24 23:52:41 +0100

    Add Section Comments

---
+++ script.js
@@ -0,0 +1,17 @@
+//====== Selecting HTML Elements ======//
+
+
+//====== Converting HTMLCollection or NodeList to Arrays ======//
+// Array-like objects (they do not have .map, .reduce, .filter)
+// HTMLCollection - List of HTML elements
+// NodeList - List of all nodes of every type (including HTML elements)
+
+
+//====== Manipulating HTML Elements ======//
+
+
+//====== Creating New HTML Elements ======//
+// 1- Create Element
+// 2- Add Content
+// 3- Select Target Parent Element
+// 4- Add Element To Parent
